d019d4a3126b33707b85143477140c50c57c2c02
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-30 18:01:50 +0530

    updated

---
+++ JS/JavaScript - From first step to professional/intro/demo.js
@@ -0,0 +1,12 @@
+let js = {
+    name: "Javascript",
+    abbreviation: "JS",
+    isAwesome: true,
+    officialSpec: "ECMAScript",
+    birthYear: 1995,
+    creator: "Brendan Eich"
+};
+
+if (js.isAwesome) {
+    shout("Hell yes JS");
+}
