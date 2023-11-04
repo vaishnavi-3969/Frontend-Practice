e7b0fa11e69a4bc7b20f04a626dcf4fea30a0705
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-05 01:50:25 +0530

    coffee masters

---
+++ Beginner/CSS/1_getting started with css/script/script.js
@@ -0,0 +1,6 @@
+function redirectToResume(){
+    window.location.href = "https://drive.google.com/file/d/17Db2g7kdBxPKUGuAI7FZIcGxp16MhsjE/view?usp=sharing";
+}
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
