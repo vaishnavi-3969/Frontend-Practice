3f5ab892158cfcd0181af48707f9096ee3d61fea
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-07 18:13:36 +0530

    Remove unnecessary files and styles

---
+++ Professional/complete_intro_to_react/intro/src/App.js
@@ -0,0 +1,31 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "dog",
+      name: "luna",
+      breed: "mavanese",
+    }),
+    React.createElement(Pet, {
+      animal: "bird",
+      name: "pepper",
+      breed: "cockatiel",
+    }),
+    React.createElement(Pet, {
+      animal: "cat",
+      name: "doink",
+      breed: "mixed",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
