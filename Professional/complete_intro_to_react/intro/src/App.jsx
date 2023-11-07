050fd5b02aba7ea04e0cb0a9545db6c5c6f25914
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-07 19:09:37 +0530

    Add Vite configuration and update ReactDOM import.

--- Professional/complete_intro_to_react/intro/src/App.jsx
+++ Professional/complete_intro_to_react/intro/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import { createRoot } from "react-dom";
 
 
 const Pet = (props) => {
@@ -31,5 +31,5 @@ const App = () => {
   ]);
 };
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
