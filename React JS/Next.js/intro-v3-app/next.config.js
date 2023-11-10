3ab27bbd1f20bdf246f84cf993de66e718271375
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-11 03:24:58 +0530

    Updated dev.db file

--- React JS/Next.js/intro-v3-app/next.config.js
+++ React JS/Next.js/intro-v3-app/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  typescript : {
+    ignoreBuildErrors: true,
+  },
+  eslint : {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
