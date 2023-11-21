3519d55a37c9bde25987e1bcbb4528a108884b32
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-11-21 22:31:17 +0530

    updated

--- Alchemy/Solidity Foundations/2_Address Interaction/4_Escrow/6_events/Escrow.sol
+++ Alchemy/Solidity Foundations/2_Address Interaction/4_Escrow/6_events/Escrow.sol
@@ -5,16 +5,19 @@ contract Escrow {
     address public depositor;
     address public beneficiary;
     address public arbiter;
-    event Approve(uint);
-    constructor(address _arbiter,  address _beneficiary) payable{
+    event Approved(uint);
+
+    constructor(address _arbiter, address _beneficiary) payable {
         beneficiary = _beneficiary;
         arbiter = _arbiter;
         depositor = msg.sender;
     }
-    function approve() external{
-        require(msg.sender==arbiter);
+
+    function approve() external {
+        require(msg.sender == arbiter, "Only the arbiter can approve");
+
         uint balanceToSend = address(this).balance;
-        beneficiary.call{value: address(this).balance}("");
-        emit Approve(balanceToSend);
+        beneficiary.call{value: balanceToSend}("");
+        emit Approved(balanceToSend);
     }
 }
