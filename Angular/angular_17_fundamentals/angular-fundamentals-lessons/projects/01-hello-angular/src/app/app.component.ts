d98b88ea9fe933516c67aa3492c8271cb68d6805
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-20 23:28:40 +0530

    angular

---
+++ Angular/angular_17_fundamentals/angular-fundamentals-lessons/projects/01-hello-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: `
+    <h1>If you are reading this...</h1>
+    <p>Things have worked out well! 🎉</p>
+  `,
+  styles: ``,
+})
+export class AppComponent {}
