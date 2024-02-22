0ab265b62c81aa5de743393a6bbd625c944a237f
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-02-22 21:58:01 +0530

    Update function to handle edge case

--- Angular/angular_17_fundamentals/angular-fundamentals-lessons/projects/06-input-output/src/app/listing/listing.component.ts
+++ Angular/angular_17_fundamentals/angular-fundamentals-lessons/projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,47 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: `
+    <article class="listing">
+      <div class="image-parent">
+        <img class="product-image" src="https://placehold.co/100x100" />
+      </div>
+      <section class="details">
+        <p class="title"><!-- car make and model-->{{car.model}} {{car.make}}</p>
+        <hr />
+        <p class="detail">
+          <span>Year</span>
+          <span><!-- year -->{{car.year}}</span>
+        </p>
+        <div class="detail">
+          <span>Transmission</span>
+          <span><!-- transmission -->Automatic</span>
+        </div>
+        <p class="detail">
+          <span>Mileage</span>
+          <span><!-- miles -->{{car.miles}}</span>
+        </p>
+        <p class="detail">
+          <span>Price</span>
+          <span><!-- price -->{{car.price}}</span>
+        </p>
+      </section>
+    </article>
+  `,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input({
+    required: true
+  }) car!: Car;
+
+}
+
+
+
+
+
+
+
+
+
