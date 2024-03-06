a4b298355b3283d70bef5ccadacacd0ab9b56831
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-03-07 04:28:11 +0530

    updated

---
+++ Python/PracticalGuideToPython/file.py
@@ -0,0 +1,6 @@
+import json
+from pprint import pprint
+
+with open("cities.json") as cities_file:
+    cities_data = json.load(cities_file)
+    pprint(cities_data)
--- Python/PracticalGuideToPython/list_comprehensions.py
+++ Python/PracticalGuideToPython/list_comprehensions.py
@@ -7,5 +7,5 @@ for name in names:
     my_list.append(name)
 print(my_list)
 
-new_list = [len(name) for name in names]
+new_list = [len(name) for name in names if len(name) > 3]
 pprint(new_list)
