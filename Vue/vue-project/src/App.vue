fe5b20577e2fa59afe1924ca601eab66e8e2d6fb
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-05-06 04:05:50 +0530

    custom components

--- Vue/vue-project/src/App.vue
+++ Vue/vue-project/src/App.vue
@@ -1,47 +1,57 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// import Counter from './components/Counter';
+
+export default {
+  components: {
+    // Counter
+  },
+  data() {
+    return {
+      displayTitle: 'Hello, Vue 3!',
+      characterList: [
+        { name: 'Vue 3', description: 'The latest version of Vue.js' },
+        {
+          name: 'Vite',
+          description:
+            'A build tool that aims to provide a faster and leaner development experience for modern web projects'
+        },
+        {
+          name: 'Pinia',
+          description: 'A Vue.js Store that is designed to be type-safe and flexible'
+        },
+        { name: 'Vue Router', description: 'The official router for Vue.js' },
+        { name: 'Vue Test Utils', description: 'Utilities for testing Vue components' },
+        {
+          name: 'Vue Dev Tools',
+          description: 'A browser extension for debugging Vue applications'
+        },
+        { name: 'Awesome Vue', description: 'A curated list of awesome things related to Vue.js' }
+      ],
+      number: 1
+    }
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ displayTitle }}</h1>
+  <ul>
+    <li v-for="character in characterList" :key="character.name">
+      <strong>{{ character.name }}</strong
+      >: {{ character.description }}
+    </li>
+  </ul>
+  <h3>Number: {{ number }}</h3>
+  <p v-if="number > 0">The number is greater than 0.</p>
+  <p v-else>The number is less than or equal to 0.</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+  color: white;
 }
 </style>
---
+++ Vue/vue-project/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    setup() {
+        const count = ref(0)
+        const increment = () => {
+            count.value++
+        }
+        const decrement = () => {
+            count.value--
+        }
+        return { count, increment, decrement }
+    }
+}
+
+</script>
+
+<template>
+    <div class="counter">
+        <h1>Counter</h1>
+        <p>Current count: {{ count }}</p>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decrement</button>
+    </div>
+</template>
