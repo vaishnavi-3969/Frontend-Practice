ff3113b9d5f3124cc71c8a8e597f6c002df1a20a
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-05-06 18:19:20 +0530

    fetching data from pokeapi

---
+++ Vue/fetch/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const pokedex = ref([1, 2, 3]);
+
+const fetchPokemon = async () => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=191');
+  const data = await response.json();
+  pokedex.value = data.results;
+};
+
+</script>
+
+<template>
+  <div>
+    <h1>New App</h1>
+    <pre>{{pokedex}}</pre>
+    <button @click="fetchPokemon">Fetch Pokemon</button>
+
+  </div>
+</template>
+
+<style scoped></style>
