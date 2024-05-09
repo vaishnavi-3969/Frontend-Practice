1e986428a127825680ebf9d1098a6f289e798c66
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-05-10 02:30:24 +0530

    server

--- Go/server/main.go
+++ Go/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,10 +10,23 @@ func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([] byte("Hello from a go program"))
 }
 
+func handleTemplate (w http.ResponseWriter, r *http.Request){
+	tmpl, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.Write([]byte ("Internal Server Error"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, "Test")
+}
 
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello",handleHello)
+	server.HandleFunc("/template", handleTemplate)
+
+	fs := http.FileServer(http.Dir("./public"))
+	server.Handle("/", fs)
 		
 	err := http.ListenAndServe(":3333", server)
 	if err != nil {
